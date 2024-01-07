4e76e8abfe6d14d8b9991d5ec873018724544219
Priya X. Pramesi <ppramesi@gmail.com>
2024-01-07 20:25:28 +0700

    header object fix

--- tweet-harvester/src/manager.ts
+++ tweet-harvester/src/manager.ts
@@ -116,7 +116,9 @@ export class RunManager {
       !_.isNil(process.env.AUTH_TOKEN) && 
       (process.env.AUTH_TOKEN as string).length > 0
     ) {
-      postCfg.headers["auth-token"] = process.env.AUTH_TOKEN;
+      postCfg.headers = {
+        "auth-token": process.env.AUTH_TOKEN
+      };
     }
 
     await axios.post(buildProcessTweetsUrl(this.processorUrl), {
