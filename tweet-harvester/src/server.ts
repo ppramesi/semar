c0201826b207ea3211abe1953e4a0532f2f176cf
Priya X. Pramesi <ppramesi@gmail.com>
2024-01-10 21:03:59 +0700

    formatting

--- tweet-harvester/src/server.ts
+++ tweet-harvester/src/server.ts
@@ -1,24 +1,19 @@
 import express, { Express, Request, Response } from "express";
 import { CrawlManager } from "./manager"; // Assuming CrawlManager is in a separate file
 import _ from "lodash";
-// import { Scheduler } from "./scheduler";
 
 export type ServerOpts = {
   crawlManager: CrawlManager;
-  // scheduler: Scheduler;
 };
 
 export class Server {
   private app: Express;
   private crawlManager: CrawlManager;
-  // private scheduler: Scheduler;
-  // private started: boolean = false;
 
   constructor(serverOpts: ServerOpts) {
     this.app = express();
     this.app.use(express.json());
     this.crawlManager = serverOpts.crawlManager;
-    // this.scheduler = serverOpts.scheduler;
     this.setupRoutes();
   }
 
@@ -43,10 +38,6 @@ export class Server {
       this.handleSearchTweets.bind(this),
     );
     this.app.post("/scrape-tweets", this.handleScrapeTweets.bind(this));
-    // this.app.post("/start", this.handleStart.bind(this));
-    // this.app.post("/pause", this.handlePause.bind(this));
-    // this.app.post("/unpause", this.handleUnpause.bind(this));
-    // this.app.post("/kill", this.handleKill.bind(this));
   }
 
   private async handleScrapeTweets(
@@ -78,32 +69,6 @@ export class Server {
       return;
     }
   }
-
-  // private async handleStart(_req: Request, res: Response): Promise<void> {
-  //   if (!this.started) {
-  //     await this.scheduler.startJob();
-  //     this.started = true;
-  //     res.status(200).json({ status: "started" });
-  //   } else {
-  //     res.status(200).json({ status: "already-started" });
-  //   }
-  // }
-
-  // private handlePause(_req: Request, res: Response): void {
-  //   this.scheduler.pause();
-  //   res.status(200).json({ status: "paused" });
-  // }
-
-  // private handleUnpause(_req: Request, res: Response): void {
-  //   this.scheduler.unpause();
-  //   res.status(200).json({ status: "unpaused" });
-  // }
-
-  // private handleKill(_req: Request, res: Response): void {
-  //   this.scheduler.endScheduler();
-  //   res.status(200).json({ status: "terminated" });
-  //   process.exit(0);
-  // }
 
   public listen(port: number | string): void {
     this.app.listen(port, () => {
